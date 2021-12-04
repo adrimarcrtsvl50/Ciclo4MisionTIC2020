ae760e5d3217fc318a66e237cc1bb7953a6c1c5a
Adriana Marcela Cortes <53945884+adrimarcrtsvl50@users.noreply.github.com>
2021-12-04 14:49:45 -0500

    cambiar jar por war

--- enbike/src/main/java/com/enbike2/enbike/EnbikeApplication.java
+++ enbike/src/main/java/com/enbike2/enbike/EnbikeApplication.java
@@ -2,24 +2,18 @@ package com.enbike2.enbike;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.context.annotation.Bean;
-import springfox.documentation.builders.PathSelectors;
-import springfox.documentation.builders.RequestHandlerSelectors;
-import springfox.documentation.spi.DocumentationType;
-import springfox.documentation.spring.web.plugins.Docket;
-import springfox.documentation.swagger2.annotations.EnableSwagger2;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
+
 
 @SpringBootApplication
-@EnableSwagger2
-public class EnbikeApplication {
-        @Bean
-  public Docket usuariosApi() {
-    return new Docket(DocumentationType.SWAGGER_2)
-        .select() 
-        .apis(RequestHandlerSelectors.any()) 
-        .paths(PathSelectors.any()) 
-        .build() ;
-            }
+
+public class EnbikeApplication extends SpringBootServletInitializer{
+       @Override
+       protected SpringApplicationBuilder configure(SpringApplicationBuilder app){
+           return app.sources(EnbikeApplication.class);
+       }
 	public static void main(String[] args) {
 		SpringApplication.run(EnbikeApplication.class, args);
 	}
